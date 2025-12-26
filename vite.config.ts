7466004230999653510db450ef29571aeefd1c6d
tpougy <thomazpougy@gmail.com>
2025-12-26 01:11:35 -0300

    chore: adjust base path for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: './',
+  base: "/audio-converter/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
     rollupOptions: {
       input: {
-        main: 'index.html',
+        main: "index.html",
       },
     },
   },
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
   optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
 });
